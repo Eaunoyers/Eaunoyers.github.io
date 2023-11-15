84282e9a4f1c1d84b99c5507c249e5d7cc5fa0ea
Eaunoyers <117490427+Eaunoyers@users.noreply.github.com>
2023-11-14 20:10:48 -0500

    ok

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function alertButton(){
     alert("JE T'AIME PLUS QUE TOUT!!")
 }
-function codesub(x){
-    print(x)
+function codesub(){
+    let code = document.getElementById("inputcode");
+    let codeval = code.value;
+    if (codeval == "Je t'aime"){
+        window.location.assign("maude.html");
+    }
+
 }
